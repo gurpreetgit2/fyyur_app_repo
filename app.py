f0da162f1aec3aaa326dab2459ef0756cfb261f2
gurpreetgit2 <gurpreet2b@gmail.com>
2024-10-27 20:29:13 +0530

    made the master work

--- app.py
+++ app.py
@@ -510,12 +510,12 @@ if not app.debug:
 #----------------------------------------------------------------------------#
 
 # Default port:
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
 
 # Or specify port manually:
-'''
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
+    app.debug = True
+    app.run(host='0.0.0.0', port=5001)
+
