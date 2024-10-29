56d05a6dbb0c9bf5d28bc3de0e6fcd14fa89a388
gurpreetgit2 <gurpreet2b@gmail.com>
2024-10-29 13:24:42 +0530

    added seacrh artist code

--- app.py
+++ app.py
@@ -307,17 +307,21 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+  # Get the search term from the form
+  search_term = request.form.get('search_term', '')
+  # Query the database for venues with names that match the search term
+  matched_artists = Artist.query.filter(Artist.name.ilike(f'%{search_term}%')).all()
+  # Format the response
+  response = {
+      "count": len(matched_artists),
+      "data": []
   }
+  # Populate response data with each venue's details
+  for artist in matched_artists:
+      response["data"].append({
+          "id": artist.id,
+          "name": artist.name
+      })
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
